2aac5577af28f41726d967528b2d7c702ff31aa4
Jakub Pyszka <kubapyszka@gmail.com>
2015-05-14 18:31:39 +0200

    txt extension

--- articles/jsonToFiles.py
+++ articles/jsonToFiles.py
@@ -3,6 +3,6 @@ def jsonToFiles(journal="oxford"):
     data = open("./" + journal + "/" + journal + ".json")
     articles = json.load(data)
     for article in articles:
-        _file = open("./" + journal + "/files/" + article["title"].encode('utf-8'), "w+")
+        _file = open("./" + journal + "/files/" + article["title"].encode('utf-8') + ".txt", "w+")
         _file.write(article["abstract"].encode('utf-8'))
         _file.close()
